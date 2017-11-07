749d8f0fd3c56bcbc487bda4eb232add66d28118
Sachin <932700352@qq.com>
2017-11-08 00:36:19 +0800

    third commit

--- app/src/main/java/com/example/chenxianqin/weatherapp/util/Utility.java
+++ app/src/main/java/com/example/chenxianqin/weatherapp/util/Utility.java
@@ -1,10 +1,13 @@
 package com.example.chenxianqin.weatherapp.util;
 
 import android.text.TextUtils;
+import android.webkit.WebView;
 
 import com.example.chenxianqin.weatherapp.db.City;
 import com.example.chenxianqin.weatherapp.db.County;
 import com.example.chenxianqin.weatherapp.db.Province;
+import com.example.chenxianqin.weatherapp.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -82,5 +85,20 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /*
+     *将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response){
+        try{
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
