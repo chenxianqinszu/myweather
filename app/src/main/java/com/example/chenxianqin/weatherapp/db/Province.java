4d6fa962d9fea8aef065bbf86da7e63bd750fb78
Sachin <932700352@qq.com>
2017-11-07 17:34:21 +0800

    Second commit.

---
+++ app/src/main/java/com/example/chenxianqin/weatherapp/db/Province.java
@@ -0,0 +1,40 @@
+package com.example.chenxianqin.weatherapp.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by chenxianqin on 2017/11/7.
+ */
+
+public class Province extends DataSupport {
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
---
+++ app/src/main/java/com/example/chenxianqin/weatherapp/util/HttpUtil.java
@@ -0,0 +1,17 @@
+package com.example.chenxianqin.weatherapp.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by chenxianqin on 2017/11/7.
+ */
+
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
